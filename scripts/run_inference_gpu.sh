ef32662cc7a5867995cb49799da7e9882a1f90d4
Tanya srivastava <130232646+Tanyasrivastava4@users.noreply.github.com>
2025-10-07 18:20:33 +0530

    run_inference_gpu.sh

--- scripts/run_inference_gpu.sh
+++ scripts/run_inference_gpu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # scripts/run_inference_gpu.sh
-MODEL_PATH="../models/llama-2-7b-q4_K_M.gguf"
-LLAMA_BIN="../llama.cpp/main"
+
+MODEL_PATH="models/llama-2-7b-q4_K_M.gguf"
+LLAMA_BIN="llama.cpp/build/bin/llama-cli"
 
 if [ -z "$1" ]; then
     echo "Usage: ./run_inference_gpu.sh 'Your prompt here'"
@@ -11,4 +12,25 @@ fi
 PROMPT="$1"
 
 echo "Running inference on GPU..."
-$LLAMA_BIN -m $MODEL_PATH -p "$PROMPT" --gpu
+$LLAMA_BIN -m $MODEL_PATH -p "$PROMPT" 
+
+
+
+
+
+
+
+#!/bin/bash
+# scripts/run_inference_gpu.sh
+#MODEL_PATH="../models/llama-2-7b-q4_K_M.gguf"
+#LLAMA_BIN="../llama.cpp/main"
+
+#if [ -z "$1" ]; then
+ #   echo "Usage: ./run_inference_gpu.sh 'Your prompt here'"
+  #  exit 1
+#fi
+
+#PROMPT="$1"
+
+#echo "Running inference on GPU..."
+#$LLAMA_BIN -m $MODEL_PATH -p "$PROMPT" --gpu
